2356e58202ec455fc80de7ef02949a086e0a8376
Altuve Mouthon,Abraham David <abrahamaltuve@unisinu.edu.co>
2021-04-13 10:48:24 -0500

    Agregar archivos de proyecto.

---
+++ PruebaLibreria/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaLibreria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unisinu.Dai.servicioPersona.listarPersonas();
+            Unisinu.Dai.servicioPersona.consultarPorCorreo("a@a.com");
+            Unisinu.Dai.servicioPersona.consultarPorDocumento("12548851");
+            Unisinu.Dai.servicioPersona.listarPersonasMismoApellido("Rodriguez");
+
+        }
+    }
+}
---
+++ Unisinu.Dai/servicioPersona.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace Unisinu.Dai
+{
+    public static class servicioPersona
+    {
+
+        public static void listarPersonas()
+        {
+            Console.WriteLine("Listar personas");
+            
+            using (personaEntities db = new personaEntities())
+            {
+                var personas = (from a in db.persona select a);
+
+                foreach(persona person in personas)
+                {
+                    string srep = person.apellido;
+                    Console.WriteLine(srep);
+                }
+                
+            }
+        }
+
+        public static void consultarPorCorreo(string email )
+        {
+            Console.WriteLine("Consultar por email");
+            using (personaEntities db = new personaEntities())
+            {
+                var personas = (from a in db.persona where a.email == email select a);
+                foreach (persona person in personas)
+                {
+                    string srep = person.apellido;
+                    Console.WriteLine(srep);
+                }
+            }
+        }
+
+        public static void consultarPorDocumento(string documento)
+        {
+            Console.WriteLine("Consultar por documento");
+            using (personaEntities db = new personaEntities())
+            {
+                var personas = (from a in db.persona where a.documento == documento select a);
+                foreach (persona person in personas)
+                {
+                    string srep = person.apellido;
+                    Console.WriteLine(srep);
+                }
+            }
+        }
+
+        public static void listarPersonasMismoApellido(string apellido)
+        {
+            Console.WriteLine("Listar personas con el mismo apellido");
+            using (personaEntities db = new personaEntities())
+            {
+                var personas = (from a in db.persona where a.apellido == apellido select a);
+                foreach (persona person in personas)
+                {
+                    string srep = person.apellido;
+                    Console.WriteLine(srep);
+                }
+            }
+        }
+    }
+}
