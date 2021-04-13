eaa5c2e063503e278bff4cf40286a72017c871bf
Altuve Mouthon,Abraham David <abrahamaltuve@unisinu.edu.co>
2021-04-13 10:53:50 -0500

    Crear diagrama de clases

---
+++ Unisinu.Dai/DiagramaPersona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unisinu.Dai
+{
+    public class DiagramaPersona
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string documento;
+        private string email;
+        private string telefono;
+    }
+}
